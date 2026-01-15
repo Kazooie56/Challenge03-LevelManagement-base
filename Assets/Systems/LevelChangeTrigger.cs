8ad756eab091b08316e5b15490169703b8153417
Kazooie56 <w0465155@campus.nscc.ca>
2026-01-15 15:38:50 -0400

    After class push

---
+++ Assets/Systems/LevelChangeTrigger.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class LevelChangeTrigger : MonoBehaviour
+{
+    public LevelManager levelManager;
+
+    private void Start()
+    {
+        levelManager = FindAnyObjectByType<LevelManager>();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("trigger is triggered");
+
+            // tell the level manager to change levels
+
+            levelManager.LevelChange();
+
+        }
+    }
+}
